c8ba6bf22d1f7850fa33c5b142dd667e1fe4054b
Gabriel <gabrielanze02@gmail.com>
2025-09-28 18:25:17 -0300

    Parte 2:tuto2 terminado

--- Assets/Prefabs/Player/Scripts/PlayerMovent.cs
+++ Assets/Prefabs/Player/Scripts/PlayerMovent.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
-public class PlayerMovent : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Vector3 forceToApply;
+    private float timeSinceLastForce;
+    private float intervalTime;
+
+    private void Start()
     {
-        
+        forceToApply = new Vector3(0, 0, 300);
+        timeSinceLastForce = 0f;
+        intervalTime = 2f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        timeSinceLastForce += Time.fixedDeltaTime;
+
+        if (timeSinceLastForce >= intervalTime)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(forceToApply);
+            timeSinceLastForce = 0f;
+        }
     }
 }
